8e7835b3619139c1011e473850435b0b8ba01fbf
Leonardo Grando <leonardo.grando@acad.unasp.edu.br>
2024-10-28 21:36:08 -0300

    darkmode

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// Toggle dark mode theme
+const toggleThemeButton = document.getElementById('toggle-theme');
+const pageTitle = document.getElementById('page-title');
+toggleThemeButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        toggleThemeButton.textContent = '☀️ Modo Claro';
+        pageTitle.style.color = '#f4f4f4';
+    } else {
+        toggleThemeButton.textContent = '🌙 Modo Escuro';
+        pageTitle.style.color = '#333';
+    }
+});
